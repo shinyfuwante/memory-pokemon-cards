1411549ab96b79cdd2d5e450ca4998f3916dcc5e
shinyfuwante <mr.philliptang@gmail.com>
2022-12-02 18:21:21 -0800

    Reload set subset on component render

--- src/components/Game/CardContainer/CardContainer.js
+++ src/components/Game/CardContainer/CardContainer.js
@@ -1,22 +1,39 @@
 import React from "react";
-// import { useState } from "react";
+import { useState } from "react";
+import { useEffect } from "react";
 import PokeCard from "../PokeCard/PokeCard";
+import PokemonLib from "../../../modules/PokemonLib/PokemonLib";
 
+const pd = PokemonLib();
 
-const CardContainer = ({input}) => {
-  // const [cardContainer, setCardContainer] = useState([]);
-  return (<>
-  {input.map(card => {
-   return (
-   <PokeCard
-   key = {card.img}
-   pokeImg = {card.img}
-   pokemon = {card.name}
-   />
-   )
-  })}
-  </>
-  )
+const CardContainer = () => {
+  const [cards, setCards] = useState([]);
+
+  useEffect(() => {
+    let isCanceled = false;
+    const genCards = async () => {
+      const [min, max] = pd.getPokeRange(6);
+      const subset = pd.getRandomSubset(5, min, max);
+      const newCards = await pd.fetchPokeInfoFromIDs(subset);
+      if (!isCanceled) {
+        setCards(newCards);
+      }
+    };
+    genCards();
+    return () => { 
+     isCanceled = true;
+    }
+  }, []);
+
+  return (
+    <>
+      {cards.map((card) => {
+        return (
+          <PokeCard key={card.img} pokeImg={card.img} pokemon={card.name} />
+        );
+      })}
+    </>
+  );
 };
 
 export default CardContainer;
