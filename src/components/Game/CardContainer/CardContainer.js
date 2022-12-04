59380b11368c740105de4bfd1a2401880099469c
shinyfuwante <mr.philliptang@gmail.com>
2022-12-03 21:46:13 -0800

    Click handler and score handlers

--- src/components/Game/CardContainer/CardContainer.js
+++ src/components/Game/CardContainer/CardContainer.js
@@ -9,6 +9,39 @@ const pd = PokemonLib();
 
 const CardContainer = () => {
   const [cards, setCards] = useState([]);
+  const [clickedTracker, setClickedTracker] = useState([]);
+  const [score, setScore] = useState(0);
+  const [bestScore, setBestScore] = useState(0);
+
+  const gameLoss = () => {
+    const newBestScore = Math.max(bestScore, score);
+    setBestScore(newBestScore);
+    setScore(0);
+    setClickedTracker([]);
+  };
+
+  const incrementScore = () => {
+    setScore((score) => score + 1);
+    const newBestScore = Math.max(bestScore, score + 1);
+    setBestScore(newBestScore);
+  };
+
+  // when a card is clicked, push the card into the array
+  // if the card is present in the array, empty the array, reset score to 0, update max score if required
+  const clickHandler = (e) => {
+    const card = e.currentTarget;
+    // use current target to avoid pushing img or text div
+    if (clickedTracker.includes(card)) {
+      // handle lose here
+      gameLoss();
+      console.log("Clicked existing card");
+    } else {
+      const newTracker = [...clickedTracker];
+      newTracker.push(card);
+      setClickedTracker(newTracker);
+      incrementScore();
+    }
+  };
 
   useEffect(() => {
     let isCanceled = false;
@@ -21,19 +54,28 @@ const CardContainer = () => {
       }
     };
     genCards();
-    return () => { 
-     isCanceled = true;
-    }
+    return () => {
+      isCanceled = true;
+    };
   }, []);
 
   return (
-    <div className="game-container">
-      {cards.map((card) => {
-        return (
-          <PokeCard key={card.img} pokeImg={card.img} pokemon={card.name} />
-        );
-      })}
-    </div>
+    <>
+      Score: {score} 
+      Best Score: {bestScore}
+      <div className="game-container">
+        {cards.map((card) => {
+          return (
+            <PokeCard
+              clickHandler={clickHandler}
+              key={card.name}
+              pokeImg={card.img}
+              pokemon={card.name}
+            />
+          );
+        })}
+      </div>
+    </>
   );
 };
 
